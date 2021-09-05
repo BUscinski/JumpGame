395c148030a6912ec7f80a6b6f0247ada6049c67
Frank Hamilton <fhamilt1@gmail.com>
2021-09-04 23:01:42 -0400

    Added Building and camera follow

---
+++ Assets/Code/Character/CameraLooseFollow.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CameraLooseFollow : MonoBehaviour
+{
+    [SerializeField] private float posLerpCoeff = 0.05f;
+    [SerializeField] private float lookAtLerpCoeff = 0.1f;
+    [SerializeField] private GameObject objectToTrack;
+    [SerializeField] private Vector3 positionOffset;
+    [SerializeField] private Vector3 lookAtOffset;
+    private Vector3 lastFinalLookAtPosition = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 lookAtTargetPosition = objectToTrack.transform.position + lookAtOffset;
+        Vector3 cameraTargetPosition = objectToTrack.transform.position + positionOffset;
+        this.transform.position = Vector3.Lerp(this.transform.position, cameraTargetPosition, posLerpCoeff);
+        Vector3  finalLookAtPosition = Vector3.Lerp(lastFinalLookAtPosition, lookAtTargetPosition, lookAtLerpCoeff);
+        lastFinalLookAtPosition = finalLookAtPosition;
+        this.transform.LookAt(lastFinalLookAtPosition);
+    }
+}
